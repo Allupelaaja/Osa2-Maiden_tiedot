80706e56d6d55d2c2412482ea2ebd65ea9cdd911
Aleksi Heikkilä <aleksi.heikkila@metropolia.fi>
2020-09-22 21:48:23 +0300

    2.14 tehty

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+const api_key = process.env.REACT_APP_API_KEY
+
 const Finder = (props) => {
   return(
     <div>
@@ -8,6 +10,44 @@ const Finder = (props) => {
       onChange={props.handler}
       />
     </div>
+  )
+}
+
+const Country = (props) => {
+  const [weather, setWeather] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get('http://api.weatherstack.com/current?access_key='+api_key+'&query='+props.results[0].capital)
+      .then(response => {
+        setWeather(response.data)
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    props.results.map(country =>
+      <div>
+        <h1>{country.name}</h1>
+        <p>capital {country.capital}</p>
+        <p>population {country.population}</p>
+        <h2>Spoken Languages</h2>
+        <ul>
+          {(country.languages.map(language =>
+          <li key={language.name}>{language.name}</li>))}
+        </ul>
+        <img src={country.flag} alt={country.name + " flag"} width="100"></img>
+        <h2>Weather in {props.results[0].capital}</h2>
+        <p><b>temperature: </b>{weather.current.temperature} Celsius</p>
+        <img src={weather.current.weather_icons} alt={"Weather icon"} width="90"></img>
+        <p><b>wind: </b>{weather.current.wind_speed} mph direction {weather.current.wind_dir}</p>
+      </div>
+    )
   )
 }
 
@@ -25,19 +65,7 @@ const Countries = (props) => {
       ))
     } else if (results.length === 1) {
       return (
-        results.map(country =>
-          <div>
-            <h1>{country.name}</h1>
-            <p>capital {country.capital}</p>
-            <p>population {country.population}</p>
-            <h2>languages</h2>
-            <ul>
-              {(country.languages.map(language =>
-              <li key={language.name}>{language.name}</li>))}
-            </ul>
-            <img src={country.flag} alt={country.name + " flag"} width="100"></img>
-          </div>
-        )
+        <Country results={results}/>
       )
     } else if (results.length === 0) {
       return (
