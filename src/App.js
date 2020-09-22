8874736c0f58e261399c109a15c276d0c4f621b4
Aleksi Heikkilä <aleksi.heikkila@metropolia.fi>
2020-09-22 19:45:14 +0300

    2.12 tehty

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Finder = (props) => {
+  return(
+    <div>
+      find countries <input
+      onChange={props.handler}
+      />
     </div>
-  );
+  )
+}
+
+const Countries = (props) => {
+  if (props.newFilter.toLowerCase() === '') {
+    return (
+    <div>
+      <p>Countries will be listed here</p>      
+    </div>)
+  } else {
+    const results = props.countries.filter(function (country) { return country.name.toLowerCase().includes(props.newFilter.toLowerCase())})
+    if (results.length > 1 && results.length <= 10) {
+      return (results.map(country =>
+        <p key={country.name}>{country.name}</p>))
+    } else if (results.length === 1) {
+      return (
+        results.map(country =>
+          <div>
+            <h1>{country.name}</h1>
+            <p>capital {country.capital}</p>
+            <p>population {country.population}</p>
+            <h2>languages</h2>
+            <ul>
+              {(country.languages.map(language =>
+              <li key={language.name}>{language.name}</li>))}
+            </ul>
+            <img src={country.flag} alt={country.name + " flag"} width="100"></img>
+          </div>
+        )
+      )
+    } else {
+      return (
+      <div>
+        <p>Too many matches ({results.length}), specify another filter</p>      
+      </div>)
+    }
+  }
+}
+
+const App = () => {
+  const [ newFilter, setNewFilter ] = useState('')
+  const [ countries, setCountries] = useState([]) 
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <Finder handler={handleFilterChange}/>
+      <Countries countries={countries} newFilter={newFilter}/>
+    </div>
+  )
 }
 
 export default App;
